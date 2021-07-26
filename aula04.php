f5d311eb9d5e6af62e672bacf389bc3f9b8bbde9
Daniel <danielivam96@gmail.com>
2021-07-26 00:17:16 -0300

    Start

---
+++ aula04.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula 04</title>
+</head>
+<body>
+    <?php
+        $n = 4;
+        $no = "Daniel";
+            
+    ?>
+</body>
+</html>
