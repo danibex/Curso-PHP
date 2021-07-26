aa2ff256d11c1d95c0f35f343e92b995adfee474
Daniel <danielivam96@gmail.com>
2021-07-26 01:17:05 -0300

    Introdução

--- aula04.php
+++ aula04.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/estilo.css">
     <title>Aula 04</title>
 </head>
 <body>
+<div>    
     <?php
-        $n = 4;
-        $no = "Daniel";
-            
+        $n = 23;
+        $no = (string)"Daniel"; 
+        /* strint ou inte (conceitos de variavel) podem ser definidas para variaveis antes delas */
+        echo $no. "<br>"; 
+        /* Concatenação abaixo (texto + variavel + texto) */
+        echo $no. " tem ". $n. " anos <br>";    
+        /* Concatenação alternativa abaxo */
+        echo "$no tem $n anos!<br>" 
+        /* Aspas simples considera tudo dentro dela como texto (inclusive variaveis) */
     ?>
+</div>
 </body>
 </html>
